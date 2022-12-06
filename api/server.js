aa106baa6d29ef117c0ae97a15210ad513354448
branden1155 <brandenp119@gmail.com>
2022-12-05 20:32:35 -0500

    updated package

--- api/server.js
+++ api/server.js
@@ -20,7 +20,13 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Database Connection Eastablished"))
 
 app.use(express.json())
-app.use('/list', listRouter )
+app.use('/api/v1/list', listRouter )
+
+app.use(express.static(path.join(__dirname, '../frontendcrud/build')));
+
+app.get('/+', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontendcrud/build', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
